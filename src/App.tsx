9f8531abaa543535a3d4def7377ca8f933548374
AJ Tran <31839316+ajtran303@users.noreply.github.com>
2023-06-16 00:22:32 -0400

    Add demo to App component of weather api function
    Just proving that it works!

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,17 @@
+// import { useEffect } from "react";
+// import fetchWeatherData from "../lib/fetch-weather-data";
+
 function App() {
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const weatherData = await fetchWeatherData("Denver");
+  //     console.log(weatherData); // will log twice because of strict mode
+  //   }
+
+  //   fetchData()
+  // }, []);
+
   return (
     <>
       <h1>Hello, Everynyan!</h1>
