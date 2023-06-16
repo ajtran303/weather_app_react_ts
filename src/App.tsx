a4c063437f9582cef8bd67a1f2d21b5789eb2282
AJ Tran <31839316+ajtran303@users.noreply.github.com>
2023-06-16 00:53:50 -0400

    Declarative programming in App to handleSubmit

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,33 @@
-// import { useEffect } from "react";
-// import fetchWeatherData from "../lib/fetch-weather-data";
+import { useState } from "react";
+import fetchWeatherData from "../lib/fetch-weather-data";
 
 function App() {
+  const [weatherData, setWeatherData] = useState(null);
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const weatherData = await fetchWeatherData("Denver");
-  //     console.log(weatherData); // will log twice because of strict mode
-  //   }
+  function handleSubmit(event: Event): void {
+    event.preventDefault();
 
-  //   fetchData()
-  // }, []);
+    const city = event.target.city.value;
+    
+    async function processWeatherData(city: string): void {
+      const weatherData = await fetchWeatherData(city);
+      setWeatherData(weatherData);
+    }
+
+    processWeatherData(city);
+    
+    event.target.reset();
+  }
 
   return (
     <>
       <h1>Hello, Everynyan!</h1>
     </>
+    // <>
+    //   <Header />
+    //   <Form onSubmit={handleSubmit} />
+    //   <Result weatherData={weatherData} />
+    // </>
   )
 }
 
